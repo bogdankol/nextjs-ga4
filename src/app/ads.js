b8ad8e9e97b17e8cef709489d1ce22cc0822c60c
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-02-20 16:26:50 +0200

    asdasd

---
+++ src/app/ads.js
@@ -0,0 +1,17 @@
+'use client'
+
+export function ads() {
+  var e=document.createElement('div');
+  e.id='neARYMwOyKtT';
+  e.innerText='I AM TEXT'
+  e.classList.add('ads')
+  // e.style.display='none';
+  document.body.appendChild(e);
+  console.log({e})
+
+  if(document.getElementById('neARYMwOyKtT')){
+    console.log('Blocking Ads: No');
+  } else {
+    console.log('Blocking Ads: Yes');
+  }
+}
