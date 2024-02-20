979e9e4e08991a389c03fe7f819e348fb72ca1a9
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-02-20 16:36:47 +0200

    asdasd

--- src/app/ads.js
+++ src/app/ads.js
@@ -5,7 +5,7 @@ export function ads() {
   e.id='neARYMwOyKtT';
   e.innerText='I AM TEXT'
   e.classList.add('ads')
-  // e.style.display='none';
+  e.style.display='none';
   document.body.appendChild(e);
   console.log({e})
 
