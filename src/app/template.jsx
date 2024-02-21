cac7de60fec50fae53edd323434d9a6d739001e3
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-02-21 14:02:10 +0200

    asdasdqwedasd

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,7 +1,7 @@
 
 'use client'
 
-import { Ads } from "./ads2"
+import { Ads } from "./(index)/ads2"
 import { AdblockDetector } from 'adblock-detector';
 
 export default function Template({ children }) {
@@ -13,7 +13,7 @@ const userHasAdblock = adbDetector.detect() // detect adblock it return ture or 
   return (
     <>
 
-      {!userHasAdblock ? <Ads /> : null}
+      {/* {!userHasAdblock ? <Ads /> : null} */}
 
       <main className="py-12 container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div className="lg:w-3/4 xl:w-2/4 mx-auto"> {children} </div>
