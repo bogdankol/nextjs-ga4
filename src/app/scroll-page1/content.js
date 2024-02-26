52fafb3e1a004d28fa889176ebd4aa8c260fa888
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-02-26 16:45:54 +0200

    asd

---
+++ src/app/scroll-page1/content.js
@@ -0,0 +1,23 @@
+'use client'
+import Link from 'next/link'
+import { useState } from 'react'
+import s from './styles.module.css'
+
+export default () => {
+	const [result, setResult] = useState(0)
+
+	return (
+		<>
+			<h1>I am scroll-page1 page</h1>
+
+			<button type='button'>I am button without any eventListener</button>
+
+			<div className={s.container}>
+				<h4>Counter</h4>
+				<button>Just to check data-sending</button>
+				<Link href={'/'}>/main-page</Link>
+			</div>
+			
+		</>
+	)
+}
---
+++ src/app/scroll-page2/page.tsx
@@ -0,0 +1,40 @@
+import Content from './content'
+
+const url = `https://nextjs-ga4.vercel.app/scroll-page2`
+const title = 'Title: scroll-page2'
+export const metadata = {
+	metadataBase: new URL(`https://nextjs-ga4.vercel.app`),
+	title,
+	description: 'description',
+	alternates: {
+		canonical: url
+	},
+	openGraph: {
+		title,
+		description: 'description',
+		url,
+		siteName: 'VyOS',
+		images: [
+			{
+				url: 'img'
+			}
+		],
+		locale: 'en_US',
+		type: 'website'
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			'max-snippet': -1,
+			'max-image-preview': 'large',
+			'max-video-preview': -1
+		}
+	}
+}
+
+export default function Page () {
+	return (
+		<Content/>
+	)
+}
