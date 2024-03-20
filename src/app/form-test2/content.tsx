f34e229cd0a391277070e9037d2d5e07fe93d3fd
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-03-20 19:31:18 +0200

    asdasd

--- src/app/form-test2/content.tsx
+++ src/app/form-test2/content.tsx
@@ -1,43 +1,63 @@
 'use client'
-import styles from "./page.module.css";
-import Link from 'next/link';
-import { useRef } from 'react';
+import styles from './page.module.css'
+import Link from 'next/link'
+import { ChangeEvent, useState } from 'react'
 
 export default function Content() {
+	const [email, setEmail] = useState<string>()
+	const [password, setPassword] = useState<string>()
+	const [textarea, setTextarea] = useState<string>()
 
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const commentRef = useRef()
-
-  const validate = () => {
-    
+	const validate = () => {
+    if(!email || !password || !textarea) throw Error('some fields are not filled')
   }
 
-  console.log({emailRef})
+	const submitHandler = (e: { preventDefault: Function }) => {
+		e.preventDefault()
+		validate()
 
-  const submitHandler = (e: {preventDefault: Function}) => {
-    e.preventDefault()
-    validate()
+		console.log('submitted')
+	}
 
-    console.log('submitted')
-  }
+	return (
+		<main className={styles.main}>
+			<div className={styles.description}>
+				<button>button to track 1</button>
+				<button>button to track 2</button>
 
-return (
-  <main className={styles.main}>
-    <div className={styles.description}>
-    <button>button to track 1</button>
-    <button>button to track 2</button>
+				<Link href={'/'}>/main-page</Link>
 
-      <Link href={'/'}>/main-page</Link>
-
-      <form autoComplete='' className={styles.form} onSubmit={submitHandler}>
-        <input ref={emailRef} className={styles.input} type='email' placeholder='type an email' />
-        <input ref={passwordRef} className={styles.input} type='password' placeholder='type an password' />
-        <input ref={commentRef} className={styles.input} type='textarea' placeholder='type a comment'/>
-        <button type='submit' className={styles.submitBtn}>submit</button>
-      </form>
-
-    </div>
-  </main>
-);
+				<form
+					autoComplete=''
+					className={styles.form}
+					onSubmit={submitHandler}
+				>
+					<input
+						className={styles.input}
+						type='email'
+						placeholder='type an email'
+            onChange={(e: any) => setEmail(e.target.value)}
+					/>
+					<input
+						className={styles.input}
+						type='password'
+						placeholder='type an password'
+            onChange={(e: any) => setPassword(e.target.value)}
+					/>
+					<input
+						className={styles.input}
+						type='textarea'
+						placeholder='type a comment'
+            onChange={(e: any) => setTextarea(e.target.value)}
+					/>
+					<button
+						type='submit'
+						className={styles.submitBtn}
+					>
+						submit
+					</button>
+				</form>
+			</div>
+		</main>
+	)
 }
